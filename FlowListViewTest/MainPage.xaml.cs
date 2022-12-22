b616c2e4cda748c0b456b39a9e53071ff565d45c
Joerg Thomas <j.thomas@peaknx.com>
2022-12-22 17:40:19 +0100

    Projektdateien hinzufügen.

---
+++ FlowListViewTest/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FlowListViewTest
+{
+    public partial class MainPage : ContentPage
+    {
+        MainPageModel pageModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            pageModel = new MainPageModel(this);
+            BindingContext = pageModel;
+        }
+        //public void FlowScrollTo(object item)
+        //{
+        //    flowListView.FlowScrollTo(item, ScrollToPosition.MakeVisible, true);
+        //}
+    }
+}
